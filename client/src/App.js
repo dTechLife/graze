12749e9eaac1dfad5afa40924f977c95e2c66b52
DestinTech <destintech@gmail.com>
2021-02-13 04:14:10 -0600

    added and implimented delete button to interface with API.

--- client/src/App.js
+++ client/src/App.js
@@ -1,122 +1,172 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 
+const url = "172.16.1.74"
 
+const size = "1.5em"
 class App extends Component {
-
- callAPI() {
-    fetch("http://localhost:9000/testAPI/readyfood/")
-        .then(res => res.json()) 
-        .then(json => this.setState({ 
-          isLoaded: 'true',
-          readyMeals: json }));
+  callAPI() {
+    fetch(`http://${url}:9000/testAPI/readyfood/`)
+      .then((res) => res.json())
+      .then((json) =>
+        this.setState({
+          isLoaded: "true",
+          readyMeals: json,
+        })
+      );
   }
 
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       readyMeals: {},
-      name: ''
-      
-
+      name: "",
+      quantity: "",
+      meal_type:"N/A"
     };
   }
-  
+
   componentDidMount() {
     this.callAPI();
   }
 
-  
-render(){
-const {  isLoaded, readyMeals, name }  = this.state;
+  render() {
+    const { isLoaded, readyMeals} = this.state;
+
+    const callAPI = () => {
+      fetch(`http://${url}:9000/testAPI/readyfood/`)
+        .then((res) => res.json())
+        .then((json) =>
+          this.setState({
+            isLoaded: "true",
+            readyMeals: json,
+          })
+        );
+    };
+
+    const handleChange = (event) => {
+      //set the state to the value of the event
+      this.setState({ [event.target.name]: event.target.value });
+    };
+
+    function createPost(opts) {
+      var x = JSON.stringify(opts);
+      console.log(x);
+      fetch(`http://${url}:9000/testAPI/readyfood`, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(opts),
+      }).then(function (response) {
+        callAPI();
+
+        return response.json();
+      });
+    }
+
+    const handleSubmit = (event) => {
+      //send post request
+      event.preventDefault();
+      let data = this.state.name;
+      let quantity = this.state.quantity;
+      let meal_type = this.state.meal_type;
+      let location = this.state.location;
+      let unit = this.state.unit;
 
 
-const callAPI = () =>{
-  fetch("http://localhost:9000/testAPI/readyfood/")
-      .then(res => res.json()) 
-      .then(json => this.setState({ 
-        isLoaded: 'true',
-        readyMeals: json }));
-}
+      console.log(`data: ${data}`);
+      if (data) {
+        createPost({
+          name: data,
+          quantity: quantity,
+          meal_type: meal_type,
+          location: location,
+          unit: unit
+        });
+      }
+    };
 
-const handleChange = (event) => { //set the state to the value of the event 
-  //console.log(this.state);
-  //console.log(event.target.value);
-  this.setState({[event.target.name]:event.target.value});
+    const handleDelete = (event) => {
+      console.log(event.target.name);
+fetch(`http://${url}:9000/testAPI/readyfood/${event.target.name}`, {
+        method: "DELETE"
+      }).then(function (response) {
+        callAPI();
 
-}
+    }
+      )}
 
-function createPost (opts){
-  var x = JSON.stringify(opts)
-  console.log (x);
-  fetch('http://localhost:9000/testAPI/readyfood',{
-    method: 'POST',
-    headers: {'Content-type': 'application/json'},
-    body: JSON.stringify(opts)
-  }).then(function(response){
-      callAPI();
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div className="App" style={{fontSize:size}}>
+        <div>
+          {readyMeals.data.map((data) => (
+            <li key={data.id}>
+              <button name={data.id} onClick={handleDelete}>X</button>
+              {data.name} | Quantity: {data.quantity} {data.unit} | 
+              added: {data.date_added} | Meal Type:{data.meal_type} | Location: {data.location}
+              </li>
+          ))}
+        </div>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <fieldset>
+              <label>
+                Name
+                <input
+                  name="name"
+                  value={this.state.value}
+                  onChange={handleChange}
+                />
+              </label><br/>
+              <label>
+                quantity
+                <input
+                  type="number"
+                  name="quantity"
+                  value={this.state.value}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                unit
+                <input
+                  name="unit"
+                  value={this.state.value}
+                  onChange={handleChange}
+                />
+              </label><br/>
+              <label>
+                type:
+                <select  style={{fontSize:"1em"}}onChange={handleChange} name="meal_type">
+                  <option value="N/A">Select one...</option>
+                  <option value="breakfast">breakfast</option>
+                  <option value="lunch">lunch</option>
+                  <option value="supper">supper</option>
+                  <option value="snack">snack</option>
+                </select>
+              </label>
 
-    return response.json();
-  })
-}
+              <label>
+                location:
+                <select  style={{fontSize:"1em"}}onChange={handleChange} name="location">
+                  <option value="N/A">Select one...</option>
+                  <option value="Fridge">Fridge</option>
+                  <option value="Deep Freeze">Deep Freeze</option>
+                  <option value="Counter">Counter</option>
+                  <option value="Fridge Freezer">Fridge Freezer</option>
+                </select>
+              </label>
 
-const handleSubmit = event => {  //send post request 
-  event.preventDefault();
-  let data = this.state.name;
-  let quantity = this.state.quantity;
-  console.log(`data: ${data}`)
-  if (data){
-    createPost({
-      name: data,
-      quantity: quantity,
-    })
+            </fieldset>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
   }
-
-  /*  let data =this.state.name;
-  console.log(`data: ${data}`)
-  alert('Form submitted: ' + this.state.name )
-  fetch('http://localhost:9000/testAPI/readyfood',{
-    method: 'POST',
-    //convert react state to JSON and send it as body of POST
-    body: data
-  }).then(function(response) {
-    console.log(response)
-    return response.json();
-    })
-*/
-//[Object: null prototype] { name: 'Cucumbers', quantity: '2' }
-}
-
-if (!isLoaded){
-  return <div>Loading...</div>
-}
-  return (
-    <div className="App">
-      <div>
-      {readyMeals.data.map (data => (
-      <li key={data.id}>{data.name} | {data.quantity} | added: {data.date_added}</li>
-      ))}
-      </div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <fieldset>
-            <label>
-              <p>Name</p>
-              <input name="name" value={this.state.value} onChange={handleChange}/>
-            </label>
-            <label>
-              <p>quantity</p>
-              <input type="number" name="quantity" value={this.state.value} onChange={handleChange}/>
-            </label>
-            
-          </fieldset>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    </div>
-  );
-}
 }
 export default App;
